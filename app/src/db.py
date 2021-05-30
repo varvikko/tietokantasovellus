f27864f8713228e6f70cf559430f0b2a85dc9d9d
Nuutti Varvikko <nvarvikko@gmail.com>
2021-05-30 22:35:01 +0300

    change heroku db url to use correct protocol

--- app/src/db.py
+++ app/src/db.py
@@ -2,5 +2,10 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace('://', 'ql://', 1)
+DATABASE_URL = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace('://', 'ql://', 1) \
+    if DATABASE_URL.startswith('postgres://') \
+    else DATABASE_URL
+
 db = SQLAlchemy(app)
