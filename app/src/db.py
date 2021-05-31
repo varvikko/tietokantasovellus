364ed32548529cb54d9a9cbcaf21a3f52c4c0579
varvikko <60470375+varvikko@users.noreply.github.com>
2021-06-01 01:58:53 +0300

    remove unnecessary line

--- app/src/db.py
+++ app/src/db.py
@@ -2,7 +2,6 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'].replace('://', 'ql://', 1)
 DATABASE_URL = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL.replace('://', 'ql://', 1) \
     if DATABASE_URL.startswith('postgres://') \
