68067275b4a3674d1dfc3ad66ae473318c26d7c7
Nuutti Varvikko <nvarvikko@gmail.com>
2021-06-01 22:18:44 +0300

    implement basic reply functionality

--- app/src/controllers/threads.py
+++ app/src/controllers/threads.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime
 from db import db
 
@@ -5,23 +6,56 @@ def create_thread(content, board, author, image_id=None):
     if not content:
         raise Exception('Content is required')
 
-    db.session.execute('''
+    result = db.session.execute('''
         INSERT INTO posts (board, author, body, image, created_at)
         VALUES (:board, :author, :body, :image, current_timestamp)
+        RETURNING id
     ''', { 'board': board, 'author': author, 'body': content, 'image': image_id })
     db.session.commit()
+
+    post_id = result.fetchone()[0]
+    reply_ids = extract_replies(content)
+    insert_replies(post_id, reply_ids)
 
 def reply(thread_id, content, author, image_id=None):
     if not content:
         raise Exception('Content is required')
     
-    db.session.execute('''
+    result = db.session.execute('''
         INSERT INTO posts (board, thread, author, body, image, created_at)
         VALUES (
             (SELECT board FROM posts WHERE id = :thread),
             :thread, :author, :body, :image, current_timestamp)
+        RETURNING id
         ''', {'thread': thread_id, 'author': author, 'body': content, 'image': image_id})
     db.session.commit()
+
+    post_id = result.fetchone()[0]
+    reply_ids = extract_replies(content)
+    insert_replies(post_id, reply_ids)
+
+def insert_replies(from_id, reply_ids):
+    if len(reply_ids) == 0:
+        return
+
+    exprs = []
+    rid = { 'from_id': from_id }
+    for i in range(len(reply_ids)):
+        exprs.append(f'(:from_id, :to_id_{i})')
+        rid[f'to_id_{i}'] = reply_ids[i]
+
+    query = f'''
+        INSERT INTO replies (from_post, to_post) VALUES
+        {','.join(exprs)}
+    '''
+
+    print(query, flush=True)
+
+    db.session.execute(query, rid)
+    db.session.commit()
+
+def extract_replies(content):
+    return re.findall(r'>>(\d+)', content)
 
 def get_threads_from_board(path, offset, count):
     result = db.session.execute('''
@@ -69,9 +103,24 @@ def construct_thread(thread_id):
                 'created_at': t[3].strftime('%d/%m/%Y %H:%M:%S'),
                 'edited': t[4],
                 'filename': t[5],
-                'author': t[6]
+                'author': t[6],
+                'replies': get_post_replies(t[0])
             },
             threads
+        )
+    )
+
+def get_post_replies(post_id):
+    result = db.session.execute('''
+        SELECT replies.from_post
+        FROM posts, replies
+        WHERE posts.id = :post_id AND posts.id = replies.to_post
+    ''', { 'post_id': post_id })
+
+    return list(
+        map(
+            lambda r: r[0],
+            result.fetchall()
         )
     )
 
--- migrate/sql/V5__add_replies.sql
+++ migrate/sql/V5__add_replies.sql
@@ -1,7 +1,7 @@
 CREATE TABLE replies (
     from_post   INTEGER NOT NULL,
     to_post     INTEGER NOT NULL,
-    PRIMARY KEY (post_from, post_to),
-    FOREIGN KEY (post_from) REFERENCES posts(id),
-    FOREIGN KEY (post_to) REFERENCES posts(id)
+    PRIMARY KEY (from_post, to_post),
+    FOREIGN KEY (from_post) REFERENCES posts(id),
+    FOREIGN KEY (to_post) REFERENCES posts(id)
 );
